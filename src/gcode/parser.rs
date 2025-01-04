b9be7d5bb3a51bfb244cbe84f63d0a59f35dbca5
Satoshi Tanaka <overthere555@gmail.com>
2025-01-05 02:03:56 +0900

    Add MPL-2.0 license notification

--- src/gcode/parser.rs
+++ src/gcode/parser.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 use core::str::FromStr;
 use nom::character::complete::{alpha1, char, digit0, digit1, not_line_ending, space0};
 use nom::IResult;
